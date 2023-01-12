5bff2eb2d296e589c0128deff15d980a62e75fc4
edduTw <eduardo.jaramillo@thoughtworks.com>
2023-01-11 21:52:47 -0500

    change tailwind.config to ok functionalitty of css in the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,ts}",
+    './components/**/*.{html,js}',
+    './shared/components/**/*.{html,js}',
+    './modules/**/*.{html,js}',
+    './src/**/*.{html,js,jsx}',
+    './index.html',
+    './src/**/*.js',
   ],
   theme: {
     extend: {},
